f90d2d0890cc74d891db672fa7f81c41f27f2d8d
Sagodi Zoltan <sagodiz@inf.u-szeged.hu>
2018-03-01 17:06:32 +0100

    tobecontinued..

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 
+
+/*
+* paramétereknek: feldolgozandó tool neve; feldolgozandó file
+*/
 int main( int argc, char** argv ) {
+  
+  Loader** loaders = {
+                      new Loader_A(),
+                      new Loader_B(),
+                      ....
+                      null
+                    };
+  
+  unsigned char i = -1;
+  while( loaders[++i] ) {
+    
+    loaders[i]->load();
+  }
+  
+  
   
   return 0;
 }
