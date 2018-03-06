25f5732dea9877f81ee619cd2b0114955494dedd
Sagodi Zoltan <sagodiz@inf.u-szeged.hu>
2018-03-06 20:57:22 +0100

    sasa

--- src/Loader_sourcemeter.cpp
+++ src/Loader_sourcemeter.cpp
@@ -151,6 +151,8 @@ bool Loader_sourcemeter::load() {
         ++i;
       } //end of while
       
+      if ( 0 == pckgClass.length() || 0 == method.length()
+      
       Record r(representation, pckgClass, method, parameterVector);
       
       if ( find( common::storedIds.begin(), common::storedIds.end(), r ) == common::storedIds.end() ) {
