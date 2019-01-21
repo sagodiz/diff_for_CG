1b7b3d3ac21797ea203d656693d02089686479f0
Sagodi Zoltan <sagodiz@inf.u-szeged.hu>
2019-01-21 10:26:58 +0100

    Update paths

--- mainRuns.sh
+++ mainRuns.sh
@@ -8,17 +8,17 @@
 
 . clean JodaLineInfoUnio
 
-####TODO frissíteni a wala útvonalakat!
-./main -s inputs/SparkSoot/Soot_v32/LangSoot32 -osa inputs/_with_lineinfo/SourceMeter_with_lineinfo/commons-lang/commons-lang.dot -sp inputs/_with_lineinfo/spoon_with_lineinfo/commons-lang/commons-lang.dot -j inputs/JCG_new/Lang/JCGLang.call -w inputs/_with_lineinfo/wala_ZeroOneContainerCFA_with_lineinfo/commons-lang/commons-lang.dot -jdt inputs/_with_lineinfo/jdt_with_lineinfo/commons-lang/commons-lang-LANG_3_7.dot -projectName LangLineInfo -methodUnio 0 -lineInfoPairing 1 -calcUnionGraph 1
+
+./main -s inputs/SparkSoot/Soot_v32/LangSoot32 -osa inputs/_with_lineinfo/SourceMeter_with_lineinfo/commons-lang/commons-lang.dot -sp inputs/_with_lineinfo/spoon_with_lineinfo/commons-lang/commons-lang.dot -j inputs/JCG_new/Lang/JCGLang.call -w inputs/_with_lineinfo/new_wala_ZeroOneContainerCFA_with_lineinfo/commons-lang.dot -jdt inputs/_with_lineinfo/jdt_with_lineinfo/commons-lang/commons-lang-LANG_3_7.dot -projectName LangLineInfo -methodUnio 0 -lineInfoPairing 1 -calcUnionGraph 1
 
 . clean LangLineInfoUnio
 
 
-./main -s inputs/SparkSoot/Soot_v32/MathSoot32 -osa inputs/_with_lineinfo/SourceMeter_with_lineinfo/commons-math/commons-math.dot -sp inputs/_with_lineinfo/spoon_with_lineinfo/commons-math/commons-math.dot -j inputs/JCG_new/Math/MathJCG.call -w inputs/_with_lineinfo/wala_ZeroOneContainerCFA_with_lineinfo/commons-math/commons-math.dot -jdt inputs/_with_lineinfo/jdt_with_lineinfo/commons-math/commons-math-MATH_3_6_1.dot -projectName MathLineInfo -methodUnio 0 -lineInfoPairing 1 -calcUnionGraph 1
+./main -s inputs/SparkSoot/Soot_v32/MathSoot32 -osa inputs/_with_lineinfo/SourceMeter_with_lineinfo/commons-math/commons-math.dot -sp inputs/_with_lineinfo/spoon_with_lineinfo/commons-math/commons-math.dot -j inputs/JCG_new/Math/MathJCG.call -w inputs/_with_lineinfo/new_wala_ZeroOneContainerCFA_with_lineinfo/commons-math.dot -jdt inputs/_with_lineinfo/jdt_with_lineinfo/commons-math/commons-math-MATH_3_6_1.dot -projectName MathLineInfo -methodUnio 0 -lineInfoPairing 1 -calcUnionGraph 1
 
 . clean MathLineInfoUnio
 
 
-./main -s inputs/SparkSoot/Soot_v32/CallESoot32 -osa inputs/_with_lineinfo/SourceMeter_with_lineinfo/istvan_example_v2/CallEdgeExamples.dot -sp inputs/_with_lineinfo/spoon_with_lineinfo/istvan_example_v2/CallEdgeExamples.dot -j inputs/JCG_new/CallEdgeExample/callEdgesExampleJCG.call -w inputs/_with_lineinfo/wala_ZeroOneContainerCFA_with_lineinfo/istvan_example/CallEdgeExample.dot -jdt inputs/_with_lineinfo/jdt_with_lineinfo/istvan_example_v2/CalledgeExample.dot -projectName CallEdgeExamplesLineInfo -methodUnio 0 -lineInfoPairing 1 -calcUnionGraph 1
+./main -s inputs/SparkSoot/Soot_v32/CallESoot32 -osa inputs/_with_lineinfo/SourceMeter_with_lineinfo/istvan_example_v2/CallEdgeExamples.dot -sp inputs/_with_lineinfo/spoon_with_lineinfo/istvan_example_v2/CallEdgeExamples.dot -j inputs/JCG_new/CallEdgeExample/callEdgesExampleJCG.call -w inputs/_with_lineinfo/new_wala_ZeroOneContainerCFA_with_lineinfo/CallEdgeExample.dot -jdt inputs/_with_lineinfo/jdt_with_lineinfo/istvan_example_v2/CalledgeExample.dot -projectName CallEdgeExamplesLineInfo -methodUnio 0 -lineInfoPairing 1 -calcUnionGraph 1
 
 . clean CallEdgeExamplesLineInfoUnio
