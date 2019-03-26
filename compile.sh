defa0238c7791e1f82630956db4153c4035e95fa
Ságodi Zoltán <sagodiz@inf.u-szeged.hu>
2019-03-26 15:22:52 +0100

    Format compile script

--- compile.sh
+++ compile.sh
@@ -22,26 +22,46 @@ if [ $# -gt 0 ]; then
   done
 fi
 
-g++ $DEF main.cpp src/Loader.cpp src/Loader_soot.cpp src/Record.cpp src/Switch.cpp src/Loader_callerhierarchy.cpp src/common.cpp src/Loader_osa.cpp src/Factory.cpp src/Loader_spoon.cpp src/Option.cpp src/OptionMethods.cpp src/Labels.cpp src/Loader_jcg.cpp src/Loader_wala.cpp src/Loader_trace.cpp src/Loader_jdt.cpp src/GraphDBCommon.cpp src/StatMethods.cpp -o main -std=c++11 -Wall -Werror
+g++ $DEF \
+main.cpp \
+src/Loader.cpp \
+src/Loader_soot.cpp \
+src/Record.cpp \
+src/Switch.cpp \
+src/common.cpp \
+src/Loader_osa.cpp \
+src/Factory.cpp \
+src/Loader_spoon.cpp \
+src/Option.cpp \
+src/OptionMethods.cpp \
+src/Labels.cpp \
+src/Loader_jcg.cpp \
+src/Loader_wala.cpp \
+src/Loader_trace.cpp \
+src/Loader_jdt.cpp \
+src/GraphDBCommon.cpp \
+src/StatMethods.cpp \
+-o main \
+-std=c++11 -Wall -Werror
 
 if [ $? -eq 0 ]; then
-  echo -e "${GREEN}Sikeres fordítás: egyesítő${NC}"
+  echo -e "${GREEN}Compilation success: comparer${NC}"
 else
-  echo -e "${RED}Sikertelen fordítás: egyesítő${NC}"
+  echo -e "${RED}Compilation failed: comparer${NC}"
 fi
 
 g++ commonTSV.cpp -std=c++11 -Wall -Werror -o commonTSV
 
 if [ $? -eq 0 ]; then
-  echo -e "${GREEN}Sikeres fordítás: táblázatos${NC}"
+  echo -e "${GREEN}Compilation success: table-generator${NC}"
 else
-  echo -e "${RED}Sikertelen fordítás: táblázatos${NC}"
+  echo -e "${RED}Compilation failed: table-generator${NC}"
 fi
 
 g++ traceValami.cpp -std=c++11 -Wall -Werror -o traceValami
 
 if [ $? -eq 0 ]; then
-  echo -e "${GREEN}Sikeres fordítás: traceValami${NC}"
+  echo -e "${GREEN}Compilation success: traceValami${NC}"
 else
-  echo -e "${RED}Sikertelen fordítás: traceValami${NC}"
+  echo -e "${RED}Compilation failed: traceValami${NC}"
 fi
